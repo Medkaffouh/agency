d12e3ba0fe0af361dd5e4bb3cf97c3da5dfc815a
Medkaffouh <kaffouhmohamed@gmail.com>
2022-08-01 18:44:15 +0100

    how to fetch data in nextjs

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,12 +1,11 @@
 import styles from "../styles/Services.module.css"
 
-const Services = () => {
+const Services = ({services}) => {
   return (
     <div className={styles.container}>
         <h1 className={styles.title}>What We Can Do?</h1>
         <h1 className={styles.subtitle}>Services we can help you with</h1>
         <div className={styles.services}>
-            
         </div>
     </div>
   )
